f5a42780429ad00c9cde55a764a8026539ac7b76
Nacereddine_rbh <llbl2153@gmail.com>
2023-01-16 09:11:39 +0100

    new fix: og:tag

--- app/head.tsx
+++ app/head.tsx
@@ -6,21 +6,6 @@ export default function Head() {
         theState: Your first and last stop for all real estate information in
         the area
       </title>
-      <meta
-        property="og:title"
-        content="theState: Your One-Stop Shop for all Real Estate Needs in the Region"
-      />
-      <meta
-        property="og:description"
-        content="We offer expert guidance on all aspects of your transaction so that you can feel secure in your decision and confident in our work."
-      />
-      <meta
-        property="og:image"
-        content="https://res.cloudinary.com/dttrs30gt/image/upload/v1673856350/theState/og_image_jhokdu.png"
-      />
-      <meta property="og:url" content="https://the-state.vercel.app" />
-      <meta property="og:type" content="website" />
-
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <html className="scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-orange-400">
+      <head>
+        <meta
+          property="og:title"
+          content="theState: Your One-Stop Shop for all Real Estate Needs in the Region"
+        />
+        <meta
+          property="og:description"
+          content="We offer expert guidance on all aspects of your transaction so that you can feel secure in your decision and confident in our work."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dttrs30gt/image/upload/v1673856350/theState/og_image_jhokdu.png"
+        />
+        <meta property="og:url" content="https://the-state.vercel.app" />
+        <meta property="og:type" content="website" />
+      </head>
       <body
         className={`bg-[#FAFAFA] ${raleway.className} relative overflow-x-hidden selection:bg-slate-300`} //i had to removed h-screen for Framer-motion to work
       >
