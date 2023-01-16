3a57aa20e46ed78fb3453c9c4f9083c1fe5ad1e6
Nacereddine_rbh <llbl2153@gmail.com>
2023-01-16 08:36:23 +0100

    adding: og:tags

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,21 @@
+import image from "../public/Images/og/og_image.png";
 export default function Head() {
   return (
     <>
       <title>
-        The State: Your first and last stop for all real estate information in
+        theState: Your first and last stop for all real estate information in
         the area
       </title>
+      <meta
+        property="og:title"
+        content="theState: Your One-Stop Shop for all Real Estate Needs in the Region"
+      />
+      <meta
+        property="og:description"
+        content="We offer expert guidance on all aspects of your transaction so that you can feel secure in your decision and confident in our work."
+      />
+      <meta property="og:image" content={image.src} />
+
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
